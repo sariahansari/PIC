7cf432ca6e39ca229a90f52a0cc516f053760729
sariahansari <sariahansari361@gmail.com>
2026-01-15 13:20:23 +0530

    second file

---
+++ p2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  char ch = 'A';
+  int num = 10;
+  float pi = 3.14;
+  double d = 10110.1234656;
+  
+  printf("The value of ch is %c\n",ch);
+  printf("The value of num is %d\n",num);
+  printf("The val;ue of pi is %.2f\n",pi);
+  printf("The value of d is %.5lf\n",d);
+
+}
